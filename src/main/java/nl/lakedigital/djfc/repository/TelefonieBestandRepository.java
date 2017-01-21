179e8aaea64a0e0e9222c88c762e5d0f29666b9f
Patrick Heidotting <patrick@lakedigital.nl>
2017-01-21 18:18:15 +0100

    Mogelijke fix

--- src/main/java/nl/lakedigital/djfc/repository/TelefonieBestandRepository.java
+++ src/main/java/nl/lakedigital/djfc/repository/TelefonieBestandRepository.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.resource.transaction.spi.TransactionStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +15,8 @@ import java.util.List;
 
 @Repository
 public class TelefonieBestandRepository {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TelefonieBestandRepository.class);
+
     @Autowired
     private SessionFactory sessionFactory;
 
@@ -55,13 +59,18 @@ public class TelefonieBestandRepository {
     public void opslaan(List<TelefonieBestand> telefonieBestands) {
         getTransaction().begin();
 
+        LOGGER.debug("Start opslaan telefonieBestands");
+
         for (TelefonieBestand telefonieBestand : telefonieBestands) {
+            LOGGER.debug(" Opslaan {}", telefonieBestand.getBestandsnaam());
             if (telefonieBestand.getId() == null) {
                 getSession().save(telefonieBestand);
             } else {
                 getSession().merge(telefonieBestand);
             }
         }
+
+        LOGGER.debug("Einde opslaan telefonieBestands");
 
         getTransaction().commit();
     }
