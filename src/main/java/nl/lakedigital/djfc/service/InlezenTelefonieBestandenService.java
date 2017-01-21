fe363fa0c7f1b25f00332f2881af21a8002b1d4c
Patrick Heidotting <patrick@lakedigital.nl>
2017-01-21 14:49:55 +0100

    extra logging

--- src/main/java/nl/lakedigital/djfc/service/InlezenTelefonieBestandenService.java
+++ src/main/java/nl/lakedigital/djfc/service/InlezenTelefonieBestandenService.java
@@ -5,6 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class InlezenTelefonieBestandenService implements Runnable {
@@ -30,7 +32,18 @@ public class InlezenTelefonieBestandenService implements Runnable {
         LOGGER.debug("Gevonden : {} bestanden", bestanden.size());
         LOGGER.debug("Al bestaand : {} bestanden", telefonieBestanden.size());
 
-        List<TelefonieBestand> nieuweBestanden = bestanden.stream().filter(file -> !telefonieBestanden.contains(new TelefonieBestand(file))).map(file -> new TelefonieBestand(file)).collect(Collectors.toList());
+        List<TelefonieBestand> nieuweBestanden = bestanden.stream().filter(new Predicate<String>() {
+            @Override
+            public boolean test(String file) {
+                LOGGER.debug("{} : {} ", file, !telefonieBestanden.contains(new TelefonieBestand(file)));
+                return !telefonieBestanden.contains(new TelefonieBestand(file));
+            }
+        }).map(new Function<String, TelefonieBestand>() {
+            @Override
+            public TelefonieBestand apply(String file) {
+                return new TelefonieBestand(file);
+            }
+        }).collect(Collectors.toList());
 
         LOGGER.debug("{} nieuwe bestanden", nieuweBestanden.size());
 
