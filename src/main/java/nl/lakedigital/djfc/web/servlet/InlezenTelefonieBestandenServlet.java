52e2907333d33d3b2948d164c3cb4f9eca80d162
Patrick Heidotting <patrick@lakedigital.nl>
2017-09-18 07:13:03 +0200

    Even uit, anders heb ik alleen maar ERROR logging

--- src/main/java/nl/lakedigital/djfc/web/servlet/InlezenTelefonieBestandenServlet.java
+++ src/main/java/nl/lakedigital/djfc/web/servlet/InlezenTelefonieBestandenServlet.java
@@ -1,15 +1,11 @@
 package nl.lakedigital.djfc.web.servlet;
 
-import nl.lakedigital.djfc.service.InlezenTelefonieBestandenService;
 import nl.lakedigital.djfc.service.TelefonieBestandService;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.inject.Inject;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 public class InlezenTelefonieBestandenServlet implements ServletContextListener {
     @Inject
@@ -19,8 +15,8 @@ public class InlezenTelefonieBestandenServlet implements ServletContextListener 
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         WebApplicationContextUtils.getRequiredWebApplicationContext(servletContextEvent.getServletContext()).getAutowireCapableBeanFactory().autowireBean(this);
 
-        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate(new InlezenTelefonieBestandenService(telefonieBestandService), 0, 5, TimeUnit.MINUTES);
+        //        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        //        scheduler.scheduleAtFixedRate(new InlezenTelefonieBestandenService(telefonieBestandService), 0, 5, TimeUnit.MINUTES);
     }
 
     public void contextDestroyed(ServletContextEvent servletContextEvent) {
